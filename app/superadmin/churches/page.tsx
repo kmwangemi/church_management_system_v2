6e283db60ab8d04539fa18708c37168943747dcf
kmwangemi <mwangemik@gmail.com>
2025-07-08 23:02:55 +0300

    feat: add profile picture url

--- app/superadmin/churches/page.tsx
+++ app/superadmin/churches/page.tsx
@@ -220,7 +220,7 @@ export default function ChurchesPage() {
           <DialogTrigger asChild>
             <Button>
               <Plus className='mr-2 h-4 w-4' />
-              Add Church & Admin Info
+              Add Church
             </Button>
           </DialogTrigger>
           <DialogContent className='max-w-2xl'>
